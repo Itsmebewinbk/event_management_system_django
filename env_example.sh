24e5f734dc00496dd32544ff74b2852fb9f22448
Itsmebewinbk <bewinbabu1998@gmail.com>
2025-06-04 06:56:44 +0530

    bug fix

--- env_example.sh
+++ env_example.sh
@@ -2,4 +2,6 @@ export SECRET_KEY="your_django_secret_key_here"
 export DB_NAME="your_database_name"
 export DB_USER="your_database_username"
 export DB_PASSWORD="your_database_password"
-export ENVIRONMENT="local"  
+export ENVIRONMENT="local"
+
+
